5bda0140b3487727f23d38aba585c4d0cddc46f0
sabine-xuaitsep <sabinepestiaux@gmail.com>
2023-06-05 19:00:25 +0200

    AppHeader component created

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import AppHeader from './components/AppHeader.vue';
 </script>
 
 <template>
-  <header>
-    <h1>Document Generator</h1>
-    <nav>
-      <RouterLink to="/">
-        <font-awesome-icon :icon="['fas', 'pencil']" />
-        Editor
-      </RouterLink>
-      <span class="navSeparator">|</span>
-      <RouterLink to="/info">
-        <font-awesome-icon :icon="['fas', 'circle-info']" />
-        Info
-      </RouterLink>
-    </nav>
-  </header>
-
+  <AppHeader />
   <RouterView />
 </template>
-
-<style scoped lang="scss">
-header {
-  color: #fefefe;
-  padding: 0 1rem;
-  text-align: center;
-
-  h1 {
-    padding-top: 1rem;
-    font-weight: 600;
-  }
-  
-  nav {
-    .navSeparator {
-      color: #818181;
-      font-weight: 900;
-    }
-
-    a {
-      color: #818181;
-      display: inline-block;
-      padding: 1rem;
-      text-decoration: none;
-
-      svg {
-        padding-right: .3rem;
-      }
-    }
-
-    a.router-link-exact-active {
-      color: #fefefe;
-    }
-
-    a:hover {
-      color: #fefefe;
-    }
-  }
-}
-</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header id="appHeader">
+    <h1>Document Generator</h1>
+    <nav>
+      <RouterLink to="/">
+        <font-awesome-icon :icon="['fas', 'pencil']" />
+        Editor
+      </RouterLink>
+      <span class="navSeparator">|</span>
+      <RouterLink to="/info">
+        <font-awesome-icon :icon="['fas', 'circle-info']" />
+        Info
+      </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  color: #fefefe;
+  padding: 0 1rem;
+  text-align: center;
+
+  h1 {
+    padding-top: 1rem;
+    font-weight: 600;
+  }
+  
+  nav {
+    .navSeparator {
+      color: #818181;
+      font-weight: 900;
+    }
+
+    a {
+      color: #818181;
+      display: inline-block;
+      padding: 1rem;
+      text-decoration: none;
+
+      svg {
+        padding-right: .3rem;
+      }
+    }
+
+    a.router-link-exact-active {
+      color: #fefefe;
+    }
+
+    a:hover {
+      color: #fefefe;
+    }
+  }
+}
+</style>
